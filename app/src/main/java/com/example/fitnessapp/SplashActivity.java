3de7dc26d4a4fe8e1fc86b0ef32465bc1bd2d51f
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2022-11-09 21:15:48 +0700

    feat: splash screen

---
+++ app/src/main/java/com/example/fitnessapp/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.fitnessapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final long DURATION = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ImageView logo = findViewById(R.id.logo);
+        logo.animate().alpha(0).setDuration(DURATION + 100);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, DURATION);
+    }
+}
