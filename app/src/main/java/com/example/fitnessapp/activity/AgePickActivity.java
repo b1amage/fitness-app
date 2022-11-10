d50792d2eb34c2fcd08fb10a4ca212ded0a8391f
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2022-11-10 13:53:07 +0700

    body index ui

--- app/src/main/java/com/example/fitnessapp/activity/AgePickActivity.java
+++ app/src/main/java/com/example/fitnessapp/activity/AgePickActivity.java
@@ -1,7 +1,11 @@
 package com.example.fitnessapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 
@@ -10,6 +14,7 @@ import com.example.fitnessapp.R;
 public class AgePickActivity extends AppCompatActivity {
 
     private NumberPicker numberPicker;
+    private Button nextBtn;
     private TextView txtAge;
     private static final int MIN_AGE = 1;
     private static final int MAX_AGE = 100;
@@ -20,6 +25,7 @@ public class AgePickActivity extends AppCompatActivity {
         setContentView(R.layout.activity_age_pick);
 
         txtAge = findViewById(R.id.txtAge);
+        nextBtn = findViewById(R.id.ageNextBtn);
         numberPicker = findViewById(R.id.agePicker);
         numberPicker.setMinValue(MIN_AGE);
         numberPicker.setMaxValue(MAX_AGE);
@@ -28,6 +34,21 @@ public class AgePickActivity extends AppCompatActivity {
             @Override
             public void onValueChange(NumberPicker numberPicker, int oldVal, int newVal) {
                 txtAge.setText(String.valueOf(newVal));
+            }
+        });
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                char gender  = getIntent().getExtras().getChar("gender");
+                Intent intent = new Intent(AgePickActivity.this, BodyIndexInputActivity.class);
+                intent.putExtra("gender", gender);
+                intent.putExtra("age", Integer.parseInt(txtAge.getText().toString()));
+
+                System.out.println("gender: " + gender);
+                System.out.println("age: " + Integer.parseInt(txtAge.getText().toString()));
+                startActivity(intent);
+                finish();
             }
         });
     }
---
+++ app/src/main/java/com/example/fitnessapp/activity/BodyIndexInputActivity.java
@@ -0,0 +1,15 @@
+package com.example.fitnessapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.fitnessapp.R;
+
+public class BodyIndexInputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_body_index_input);
+    }
+}
